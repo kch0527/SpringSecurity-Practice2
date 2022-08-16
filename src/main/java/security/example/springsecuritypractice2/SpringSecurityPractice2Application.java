f10bbb1809ec06ab64f36cc87485a2463400e8bc
chan <jhgu128@naver.com>
2022-08-16 23:25:42 +0900

    first commit

---
+++ src/main/java/security/example/springsecuritypractice2/SpringSecurityPractice2Application.java
@@ -0,0 +1,13 @@
+package security.example.springsecuritypractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPractice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/security/example/springsecuritypractice2/SpringSecurityPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package security.example.springsecuritypractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
